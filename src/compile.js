bafe1e94c6ac59e25631733fb21f1785c492c749
Unknown <usfirepilot123@gmail.com>
2019-01-01 17:11:14 -0700

    New Setup

--- src/compile.js
+++ src/compile.js
@@ -1,5 +1,6 @@
 const { compile } = require("nexe");
 const argv = require('minimist')(process.argv.slice(2));
+const package = require("../package.json");
 
 (async () => {
   await compile({
@@ -7,6 +8,16 @@ const argv = require('minimist')(process.argv.slice(2));
     build: true,
     ico: "./icon.ico",
     name: "./release/ATSRichPresenceCMD",
-    clean: argv.clean ? true : false
+    clean: argv.clean ? true : false,
+    rc: {
+      "CompanyName": "FireController#1847",
+      "ProductName": "ATS Rich Presence",
+      "FileDescription": package.description,
+      "FileVersion": package.version,
+      "ProductVersion": package.version,
+      "OriginalFilename": "ATSRichPresence.exe",
+      "InternalName": "ATSRichPresence",
+      "LegalCopyright": "Copyright FireController#1847."
+    }
   });
 })();
