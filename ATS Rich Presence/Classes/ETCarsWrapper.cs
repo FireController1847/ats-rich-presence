7d05fcdb85fbe13b497fd0e63d1c9bfb637d50b3
FireController#1847 <usfirepilot123@gmail.com>
2020-01-17 18:04:08 -0700

    Add Basic ETCars Connecting & Setup Workspace

---
+++ ATS Rich Presence/Classes/ETCarsWrapper.cs
@@ -0,0 +1,50 @@
+﻿using ETCarsDotNetSdk;
+using ETCarsDotNetSdk.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ATSRP {
+    public class ETCarsWrapper : ETCarsClient {
+
+        private readonly int[] delays = {
+            1000, 2000, 5000, 10000, 30000
+        };
+        private bool autoReconnect = true;
+        private int attempts = 0;
+
+        // Add event forwarding
+        public new event ETCarsConnectionFailedEventArgs Errored;
+
+        /// <summary>
+        /// Wraps the ETCarsClient and makes it continually attempt to connect until the user specified it should stop.
+        /// </summary>
+        public ETCarsWrapper() : base() {
+            base.Errored += OnErrored;
+        }
+
+        /// <summary>
+        /// Enables automatically reconnecting to the API.
+        /// </summary>
+        public void EnableAutoReconnect() {
+            autoReconnect = true;
+        }
+
+        /// <summary>
+        /// Disabled automatically reconnecting to the API.
+        /// </summary>
+        public void DisableAutoReconnect() {
+            autoReconnect = false;
+        }
+
+        private void OnErrored(ETCarsConnectionFailedArgs args) {
+            this.Errored.Invoke(args);
+            if (autoReconnect) {
+                Thread.Sleep(delays[attempts < delays.Length ? attempts++ : attempts]);
+                base.Connect();
+            }
+        }
+
+    }
+}
---
+++ ATS Rich Presence/Program.cs
@@ -0,0 +1,40 @@
+﻿using DiscordRPC;
+using ETCarsDotNetSdk;
+using ETCarsDotNetSdk.Events;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ATSRP {
+
+    public class Program {
+
+        private const string DISCORD_APPLICATION_ID = "529091171633594368"; // TODO: Put into a configuration file?
+        private static DiscordRpcClient rpc = new DiscordRpcClient(DISCORD_APPLICATION_ID);
+        private static ETCarsWrapper et = new ETCarsWrapper();
+
+        public static void Main(string[] args) {
+            Console.WriteLine("Connecting to ETCars...");
+            et.Connected += OnEtCarsConnected;
+            et.Errored += OnEtCarsErrored;
+            et.Disconnected += OnEtCarsDisconnected;
+            et.Connect();
+
+            Console.ReadKey();
+        }
+
+        public static void OnEtCarsConnected() {
+            Console.WriteLine("ETCars Connected");
+        }
+
+        public static void OnEtCarsErrored(ETCarsConnectionFailedArgs args) {
+            Console.WriteLine("Awaiting connection to ETCars...");
+        }
+
+        public static void OnEtCarsDisconnected() {
+            Console.WriteLine("ETCars Disconnected");
+        }
+
+    }
+
+}
